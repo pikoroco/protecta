a0038e95aba48136eb9ac9f2a53940fec3c14b5c
Kovács Roland <pikoroco@gmail.com>
2021-08-27 18:06:08 +0200

    Add Author and Publication factories to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Publication;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::factory(10)->create();
+         Author::factory(5)->create();
+         Publication::factory(15)->create();
     }
 }
