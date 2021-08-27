ebe38f80428db11742e98863ac25decb31cf5879
Kovács Roland <pikoroco@gmail.com>
2021-08-27 18:05:10 +0200

    add relationship to Author model

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Author extends Model
 {
     use HasFactory;
+
+    /**
+     * @return HasMany
+     */
+    public function publications(): HasMany
+    {
+        return $this->hasMany(Publication::class);
+    }
 }
