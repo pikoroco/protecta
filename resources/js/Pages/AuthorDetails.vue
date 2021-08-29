e615e1820cdbd6c247e81afe19feaafd0134b3e9
Kovács Roland <pikoroco@gmail.com>
2021-08-30 00:22:55 +0200

    Add author vues

---
+++ resources/js/Pages/AuthorDetails.vue
@@ -0,0 +1,126 @@
+<template>
+    <app-layout title="Publications">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Author details page
+            </h2>
+        </template>
+
+        <div class="py-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                        <div class="px-4 py-5 sm:px-6">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                Author detail
+                            </h3>
+                            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                                Personal details of author.
+                            </p>
+                            <jet-nav-link :href="route('author.edit', author.id)" class="btn btn-warning w-24 px-6 mx-2">Edit</jet-nav-link>
+                            <jet-nav-link :href="route('author.destroy', author.id)" class="btn btn-error w-24 px-6">Delete</jet-nav-link>
+                        </div>
+                        <div class="border-t border-gray-200">
+                            <dl>
+                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">
+                                        Full name
+                                    </dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {{ author.name }}
+                                    </dd>
+                                </div>
+                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">
+                                        Address
+                                    </dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {{ author.address }}
+                                    </dd>
+                                </div>
+                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">
+                                        Description
+                                    </dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        {{ author.description }}
+                                    </dd>
+                                </div>
+                            </dl>
+                        </div>
+                    </div>
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Id
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Date
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Title
+                                        </th>
+                                        <th scope="col" class="relative text-right px-6 py-3">
+                                            <jet-nav-link class="btn btn-accent w-24" :href="route('author.create')">New</jet-nav-link>
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200" v-for="publication in author.publications">
+                                    <tr>
+<!--                                            ID-->
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{ publication.id }}
+                                            </div>
+                                        </td>
+<!--                                            Date-->
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{ publication.date }}
+                                            </div>
+                                        </td>
+<!--                                            Title-->
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                {{ publication.title }}
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <jet-nav-link class="btn btn-secondary w-24" :href="route('author.destroy', { author: author.id })">Delete</jet-nav-link>
+                                        </td>
+                                    </tr>
+
+                                    <!-- More people... -->
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+
+import AppLayout from "../Layouts/AppLayout";
+import JetNavLink from "../Jetstream/NavLink";
+
+export default {
+    name: "AuthorDetails",
+    props: ['author'],
+    components: {
+        AppLayout,
+        JetNavLink,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/AuthorIndex.vue
@@ -0,0 +1,93 @@
+<template>
+    <app-layout title="Authors">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Authors page
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="px-6 py-6">
+                    <jet-nav-link class="btn btn-accent w-24" :href="route('author.create')">
+                        New
+                    </jet-nav-link>
+                </div>
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Id
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Name
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Address
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Number of publications
+                                        </th>
+                                        <th scope="col" class="relative px-6 py-3">
+                                            <span class="sr-only">Delete</span>
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200" v-for="author in authors">
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                            {{ author.id }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    <jet-nav-link :href="route('author.show', { author: author.id })">
+                                                        {{ author.name }}
+                                                    </jet-nav-link>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            {{ author.address }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            {{ author.publications.length }}
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <jet-nav-link class="btn btn-secondary w-24" :href="route('author.destroy', { author: author.id })">
+                                                Delete
+                                            </jet-nav-link>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import AppLayout from "../Layouts/AppLayout";
+import JetNavLink from "../Jetstream/NavLink";
+
+export default {
+    name: "AuthorIndex",
+    props: ['authors'],
+    components: {
+        AppLayout,
+        JetNavLink
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
