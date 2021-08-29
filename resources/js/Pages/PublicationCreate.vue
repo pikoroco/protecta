05b2a8da462b8e0972e4480c53368ccbfdd0c937
Kovács Roland <pikoroco@gmail.com>
2021-08-30 00:23:07 +0200

    Add publication vues

---
+++ resources/js/Pages/PublicationCreate.vue
@@ -0,0 +1,58 @@
+<template>
+    <app-layout title="Author Create">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Author create page
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <form :action="route('author.store')" method="POST">
+                    @csrf
+                    <div class="shadow overflow-hidden sm:rounded-md">
+                        <div class="px-4 py-5 bg-white sm:p-6">
+                            <div class="grid gap-6">
+                                <div class="col-span-12">
+                                    <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                                    <input type="text" name="name" id="name" autocomplete="name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-12">
+                                    <label for="address" class="block text-sm font-medium text-gray-700">Address</label>
+                                    <input type="text" name="address" id="address" autocomplete="address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-12">
+                                    <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+                                    <input type="text" name="description" id="description" autocomplete="description" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+
+import AppLayout from "../Layouts/AppLayout";
+export default {
+    name: "PublicationCreate",
+    props: ['csrf_field'],
+    components: {
+        AppLayout,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
